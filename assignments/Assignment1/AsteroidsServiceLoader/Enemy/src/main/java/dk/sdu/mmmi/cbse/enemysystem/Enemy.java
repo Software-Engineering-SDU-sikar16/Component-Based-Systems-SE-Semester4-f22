c3a81583bb70b8a3f1b47dd2b915b7aff4eacb58
Simon <simonkaring@gmail.com>
2022-04-07 14:52:13 +0200

    Updates

---
+++ assignments/Assignment1/AsteroidsServiceLoader/Enemy/src/main/java/dk/sdu/mmmi/cbse/enemysystem/Enemy.java
@@ -0,0 +1,13 @@
+package dk.sdu.mmmi.cbse.enemysystem;
+
+import dk.sdu.mmmi.cbse.common.data.Entity;
+import dk.sdu.mmmi.cbse.common.data.entityparts.MovingPart;
+
+/**
+ *
+ * @author sikar16
+ */
+
+public class Enemy extends Entity {
+
+}
--- projects/SB4-KOM-F20-sikar16/AsteroidsServiceLoader/Enemy/src/main/java/dk.sdu.mmi.cbse.enemysystem/Enemy.java
+++
@@ -1,7 +0,0 @@
-package dk.sdu.mmi.cbse.enemysystem;
-
-import dk.sdu.mmmi.cbse.common.data.Entity;
-
-public class Enemy extends Entity {
-
-}
